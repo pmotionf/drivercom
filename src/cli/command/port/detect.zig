1b08929f5247c4c6ad7796119fda07e28b6c4785
mochalins <117967760+mochalins@users.noreply.github.com>
2024-09-06 11:02:12 +0900

    feat: Clearer detect results printing

--- src/cli/command/port/detect.zig
+++ src/cli/command/port/detect.zig
@@ -11,6 +11,8 @@ pub fn execute(_: @This()) !void {
     var random = std.Random.DefaultPrng.init(0);
     const random_connection_seed = random.random().int(u32);
 
+    var attempted_connections: usize = 0;
+
     while (try port_iterator.next()) |_port| {
         if (comptime builtin.os.tag == .linux) {
             if (_port.display_name.len < "/dev/ttyUSBX".len) {
@@ -28,6 +30,7 @@ pub fn execute(_: @This()) !void {
             "Attempting connection with COM port: {s}",
             .{_port.display_name},
         );
+        attempted_connections += 1;
 
         // Attempt connection.
         var port = std.fs.cwd().openFile(_port.file_name, .{
@@ -88,10 +91,13 @@ pub fn execute(_: @This()) !void {
 
         if (connection_made) {
             const stdout = std.io.getStdOut().writer();
-            try stdout.print("{s}\n", .{_port.display_name});
+            try stdout.print("\nConnection found: {s}\n", .{_port.display_name});
             return;
         }
     }
     const stderr = std.io.getStdErr().writer();
-    try stderr.print("No PMF Smart Driver COM port detected.\n", .{});
+    try stderr.print(
+        "{s}No PMF Smart Driver COM port detected.\n",
+        .{if (attempted_connections > 0) "\n" else ""},
+    );
 }
