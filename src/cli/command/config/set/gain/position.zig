067037f14fe75b777e72943c220e578e68a0358f
mochalins <117967760+mochalins@users.noreply.github.com>
2024-10-02 17:08:46 +0900

    feat: No config file required in position gain set

--- src/cli/command/config/set/gain/position.zig
+++ src/cli/command/config/set/gain/position.zig
@@ -30,10 +30,10 @@ pub fn help(_: @This()) !void {
 }
 
 pub fn execute(self: @This()) !void {
-    const name = self.file orelse {
-        std.log.err("file must be provided", .{});
+    if (cli.port == null and self.file == null) {
+        std.log.err("serial port or file must be provided", .{});
         return;
-    };
+    }
 
     if (cli.positionals.len != 2) {
         std.log.err(
@@ -56,35 +56,80 @@ pub fn execute(self: @This()) !void {
 
     const axis_index = axis_id - 1;
 
-    var file = try std.fs.cwd().openFile(name, .{ .mode = .read_write });
-    defer file.close();
+    if (self.file) |name| {
+        var file = try std.fs.cwd().openFile(name, .{ .mode = .read_write });
+        defer file.close();
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        const allocator = arena.allocator();
 
-    const file_str = try file.readToEndAlloc(allocator, 1_024_000_000);
-    defer allocator.free(file_str);
-    var untyped = try yaml.Yaml.load(
-        allocator,
-        file_str,
-    );
-    defer untyped.deinit();
-    var config = try untyped.parse(drivercom.Config);
+        const file_str = try file.readToEndAlloc(allocator, 1_024_000_000);
+        defer allocator.free(file_str);
+        var untyped = try yaml.Yaml.load(
+            allocator,
+            file_str,
+        );
+        defer untyped.deinit();
+        var config = try untyped.parse(drivercom.Config);
 
-    const axis = &config.axes[axis_index];
+        const axis = &config.axes[axis_index];
 
-    axis.position_gain = config.calcPositionGain(
-        axis_index,
-        denominator,
-    );
+        axis.position_gain = config.calcPositionGain(
+            axis_index,
+            denominator,
+        );
 
-    try file.seekTo(0);
-    try yaml.stringify(allocator, config, file.writer());
+        try file.seekTo(0);
+        try yaml.stringify(allocator, config, file.writer());
+    }
 
     if (cli.port) |_| {
+        var config: drivercom.Config = undefined;
+
         var sequence: u16 = 0;
         var msg = drivercom.Message.init(
+            .get_current_gain_denominator,
+            sequence,
+            axis_index,
+        );
+        while (true) {
+            try command.sendMessage(&msg);
+            const rsp = try command.readMessage();
+            if (rsp.kind == .set_current_gain_denominator and
+                rsp.sequence == sequence)
+            {
+                const payload = rsp.payload(.set_current_gain_denominator);
+                config.axes[axis_index].current_gain.denominator =
+                    payload.denominator;
+                break;
+            }
+        }
+
+        sequence += 1;
+        msg = drivercom.Message.init(
+            .get_velocity_gain_denominator,
+            sequence,
+            axis_index,
+        );
+        while (true) {
+            try command.sendMessage(&msg);
+            const rsp = try command.readMessage();
+            if (rsp.kind == .set_velocity_gain_denominator and
+                rsp.sequence == sequence)
+            {
+                const payload = rsp.payload(.set_velocity_gain_denominator);
+                config.axes[axis_index].velocity_gain.denominator =
+                    payload.denominator;
+                break;
+            }
+        }
+
+        const axis = &config.axes[axis_index];
+        axis.position_gain = config.calcPositionGain(axis_index, denominator);
+
+        sequence += 1;
+        msg = drivercom.Message.init(
             .set_position_gain_p,
             sequence,
             .{ .axis = axis_index, .p = axis.position_gain.p },
