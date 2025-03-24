2ff98c4a4f846ddaeddb1bab172f2d96d505abb0
mochalins <117967760+mochalins@users.noreply.github.com>
2025-03-24 19:20:22 +0900

    scm: 0.4.1

--- src/drivercom.zig
+++ src/drivercom.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 /// `drivercom` version; always matches version in `build.zig.zon`.
-pub const version = std.SemanticVersion.parse("0.4.0") catch unreachable;
+pub const version = std.SemanticVersion.parse("0.4.1") catch unreachable;
 
 pub const Config = @import("Config.zig");
 pub const OldConfig = @import("OldConfig.zig");
